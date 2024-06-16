0c87e593fc879c5dc51a351bcd1ef960905d4d31
Pan_Janke <jan.krol.legowski@gmail.com>
2024-06-17 01:55:15 +0200

    type safe use selector hook

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,7 @@
-import {useDispatch} from 'react-redux';
-import {AppDispatch} from "./store.ts";
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from "./store.ts";
 
 type DispatchFunction = () => AppDispatch;
 
 export const useCartDispatch: DispatchFunction = useDispatch;
+export const useCartSelector:TypedUseSelectorHook<RootState> = useSelector;
